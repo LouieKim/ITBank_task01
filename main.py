cff097a558ca94296b46c381cbd8bade92ad3cef
LouieKim <hyeok0724.kim@gmail.com>
2019-01-24 14:44:12 +0900

    190124 1444

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import datetime
 import os
 import getpass
+import logging
 
 if __name__ == "__main__":
     # writedata.py
-    print("write txt")
+    logging.basicConfig(format='%(asctime)s %(message)s', datefmt='%m/%d/%y %I:%M:%S %p', filename='example.log', level=logging.DEBUG)
+    logging.info('Make write main file')
     current_id = str(getpass.getuser())
     file_path = "/home/" + current_id + "/writed_main.txt"
     f = open(file_path, 'w')
     f.write(str(datetime.datetime.now()))
     f.close()
-    print("Save file")
+    logging.info('Done writed main file')
